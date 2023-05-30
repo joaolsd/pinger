19ebfea73846ad9a8c9728204a8aeec06deaefbe
joaolsd <joao@bondis.org>
2023-05-30 18:16:52 +0200

    Add batch script for running all tests in sequence

---
+++ batch.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+./listener  -i eno1 -k  203.133.248.122 -l 2401:2000:6660::122  -o yarrp-noeh.log &
+PID=$!
+./sender -f ./yarrp-noeh.csv -4 203.133.248.122 -6 2401:2000:6660::122 -i eno1 -a 14:18:77:43:b9:b8 -b 00:00:0c:9f:f4:59 -c 00:05:73:a0:08:41
+kill $PID
+HOSTNAME=hostname
+
+for size in 8 16 32 64 128
+do
+  ./listener  -i eno1 -k  203.133.248.122 -l 2401:2000:6660::122  -o yarrp-$HOSTNAME-hbh$size.log &
+  LISTEN_PID=$!
+  tcpdump -nvvvv -w pkts.pcap -i eno1  'dst port 443 or  icmp6[0]==3' &
+  TCPDUMP_PID=$!
+  ./sender -f ./yarrp-hbh$size.csv -4 203.133.248.122 -6 2401:2000:6660::122 -i eno1 -a 14:18:77:43:b9:b8 -b 00:00:0c:9f:f4:59 -c 00:05:73:a0:08:41
+  kill $LISTEN_PID
+done
+
+for size in 8 16 32 64 128
+do
+  ./listener  -i eno1 -k  203.133.248.122 -l 2401:2000:6660::122  -o yarrp-$HOSTNAME-dst$size.log &
+  LISTEN_PID=$!
+
+  ./sender -f ./yarrp-dst$size.csv -4 203.133.248.122 -6 2401:2000:6660::122 -i eno1 -a 14:18:77:43:b9:b8 -b 00:00:0c:9f:f4:59 -c 00:05:73:a0:08:41
+  kill $LISTEN_PID
+done
